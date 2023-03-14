752c63e0180f13de79b15e335ca6c642e5b7148d
xxparthparekhxx <xx.parthparekh.xx@gmail.com>
2023-03-14 11:16:16 -0500

    added better seo

--- next-seo.config.js
+++ next-seo.config.js
@@ -6,11 +6,21 @@ export default {
       locale: 'en_IE',
       url: 'https://xxparthparekhxx.vercel.app/',
       siteName: 'xxparthparekhxx',
+      images: [
+        {
+          url: 'https://xxparthparekhxx.vercel.app/parth.jpeg',
+          width: 553,
+          height: 553,
+          alt: 'Profile Photo',
+        },
+      ],
     },
     twitter: {
       handle: '@parthparekhx',
       site: '@XxParthparekhxX',
       cardType: 'summary_large_image',
+     
     },
+    
     
   };
