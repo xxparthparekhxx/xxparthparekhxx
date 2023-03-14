ea96312d5152cb0d7753226227910a38937a4993
xxparthparekhxx <xx.parthparekh.xx@gmail.com>
2023-03-14 11:25:33 -0500

    making seo better

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,26 +1,34 @@
 export default {
-    title:"Parth Parekh",
-    description:"Fullstack Developer and AI Student at Georgian College with a Drive for Creating Cutting-Edge Applications",
+    title: "Parth Parekh",
+    description: "Fullstack Developer and AI Student at Georgian College with a Drive for Creating Cutting-Edge Applications",
     openGraph: {
-      type: 'website',
-      locale: 'en_IE',
-      url: 'https://xxparthparekhxx.vercel.app/',
-      siteName: 'xxparthparekhxx',
-      images: [
-        {
-          url: 'https://xxparthparekhxx.vercel.app/parth.jpeg',
-          width: 553,
-          height: 553,
-          alt: 'Profile Photo',
-        },
-      ],
+        type: 'website',
+        locale: 'en_IE',
+        url: 'https://xxparthparekhxx.vercel.app/',
+        siteName: 'xxparthparekhxx',
+        images: [
+            {
+                url: 'https://xxparthparekhxx.vercel.app/parth.jpeg',
+                width: 553,
+                height: 553,
+                alt: 'Profile Photo',
+            },
+        ],
     },
     twitter: {
-      handle: '@parthparekhx',
-      site: '@XxParthparekhxX',
-      cardType: 'summary_large_image',
-     
+        handle: '@parthparekhx',
+        site: '@XxParthparekhxX',
+        cardType: 'summary_large_image',
+        images: [
+            {
+                url: 'https://xxparthparekhxx.vercel.app/parth.jpeg',
+                width: 553,
+                height: 553,
+                alt: 'Profile Photo',
+            },
+        ],
+
     },
-    
-    
-  };
+
+
+};
