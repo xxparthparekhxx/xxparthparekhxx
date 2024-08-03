e8e9a76344e32e95a837f935ec72bc0f2c5a1896
--global <--global>
2024-08-03 13:57:57 +0530

    added sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,5 @@
+module.exports = {
+    siteUrl: 'https://parthp.in', // Replace with your actual domain
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+}
---
+++ pages/api/sitemap.ts
@@ -0,0 +1,69 @@
+// pages/sitemap.xml.js
+import { getServerSideSitemap } from "next-sitemap";
+import { NextApiRequest, NextApiResponse } from 'next';
+import {
+  fetchProjects,
+  fetchProjectsByStack,
+  fetchStacks,
+  fetchWorkExperiences,
+  fetchPosts,
+} from "../../src/api";
+
+
+const DOMAIN = 'https://parthp.in'; // Your domain
+
+function generateSitemapXml(pages:{loc:string,lastmod:string}[]) {
+  let xml = '<?xml version="1.0" encoding="UTF-8"?>\n';
+  xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
+
+  pages.forEach(page => {
+    xml += '  <url>\n';
+    xml += `    <loc>${DOMAIN}${page.loc}</loc>\n`;
+    xml += `    <lastmod>${page.lastmod}</lastmod>\n`;
+    xml += '    <changefreq>weekly</changefreq>\n';
+    xml += '    <priority>0.8</priority>\n';
+    xml += '  </url>\n';
+  });
+
+  xml += '</urlset>';
+  return xml;
+}
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  // Method to get all of your site's pages
+  const getAllPages = async () => {
+    // This is where you'd fetch all your site's pages
+    // For example, from your database or API
+
+    let [projects, experiences, posts] = await Promise.all([
+      await fetchProjects(),
+      await fetchWorkExperiences(),
+      await fetchPosts(),
+    ]);
+    let endpoints = [
+      { url: "/", lastModified: new Date() },
+      { url: "/Contact", lastModified: new Date() },
+      { url: "/Blog", lastModified: new Date() },
+      ...projects.map((e) => {
+        return { url: "/Work/" + e.id, lastModified: new Date() };
+      }),
+      ...experiences.map((e) => {
+        return { url: "/Experience/" + e.id, lastModified: new Date() };
+      }),
+      ...posts.map((e) => {
+        return { url: "/Blog/" + e.id, lastModified: new Date() };
+      }),
+    ];
+
+    return endpoints;
+  };
+
+  const pg = await getAllPages();
+
+  const pages = pg.map((page) => ({
+    loc: `https://parthp.in${page.url}`, // Replace with your actual domain
+    lastmod: page.lastModified.toISOString(),
+    // You can add more fields like changefreq, priority, etc.
+  }));
+
+  return  res.status(200).send(generateSitemapXml(pages));
+};
