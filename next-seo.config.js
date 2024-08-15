7be5fd905ee27adde0703bd87c4c6cc05a1c6b6a
--global <--global>
2024-08-15 17:32:25 +0530

    added better seo

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,27 +1,37 @@
 export default {
-    title: "Parth Parekh",
-    description: "Fullstack Developer ",
+    title: "Parth Parekh | Fullstack Developer & AI Specialist",
+    description: "Experienced Fullstack Developer with a diploma in AI, specializing in web development, JavaScript, React, Node.js, and artificial intelligence. Portfolio and professional services of Parth Parekh.",
     openGraph: {
-        type: 'website',
-        locale: 'en_IE',
-        url: 'https://parthp.in/',
-        siteName: 'xxparthparekhxx',
-        images: [
-            {
-                url: 'https://parthp.in/parth.png',
-                width: 553,
-                height: 553,
-                alt: 'My photo',
-            },
-        ],
+      type: 'website',
+      locale: 'en_US',
+      url: 'https://parthp.in/',
+      siteName: 'Parth Parekh - Fullstack Developer & AI Specialist',
+      images: [
+        {
+          url: 'https://parthp.in/parth.png',
+          width: 553,
+          height: 553,
+          alt: 'Parth Parekh - Fullstack Developer & AI Specialist',
+        },
+      ],
     },
     twitter: {
-        handle: '@parthparekhx',
-        site: '@XxParthparekhxX',
-        cardType: 'summary_large_image',
+      handle: '@parthparekhx',
+      site: '@parthparekhx',
+      cardType: 'summary_large_image',
     },
-};
-
+    additionalMetaTags: [
+      {
+        name: 'keywords',
+        content: 'Parth Parekh, Fullstack Developer, AI Specialist, Artificial Intelligence, Web Development, JavaScript, React, Node.js, Software Engineer, Machine Learning, xxparthparekhxx, python, Flutter, React Naive, tailwind, graphql, django, dart, docker, lua'
+      },
+      {
+        name: 'author',
+        content: 'Parth Parekh'
+      }
+    ],
+    canonical: 'https://parthp.in/',
+  };
 export const QrSeo =  {
     title: "Qr Code Genreator (free)",
     description: "Simple app to generate qr code copy and download them free",
